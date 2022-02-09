6354b15bb4cfef7269fac35829e63d0936de4097
ERLMKea <erlm@kea.dk>
2022-02-09 12:41:20 +0100

    Initial commit

---
+++ src/test/java/com/example/xsmittetryk02/XSmittetryk02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xsmittetryk02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XSmittetryk02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
